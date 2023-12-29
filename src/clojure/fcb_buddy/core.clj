c511596f564180e7b976998ba085362482a974e6
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2023-12-29 10:47:15 -0600

    Remove seesaw lib; update menu & tab pane

--- src/clojure/fcb_buddy/core.clj
+++ src/clojure/fcb_buddy/core.clj
@@ -1,15 +1,11 @@
 (ns fcb-buddy.core
   "Application frames & entry point."
-  (:use [seesaw.core]
-        [seesaw.widgets.log-window])
   (:require [clojure.core.cache :as cache]
             [clojure.java.browse :refer [browse-url]]
             [clojure.main :as m]
             [clojure.pprint :as pp]
             [clojure.string :as str]
             [cljfx.api :as fx]
-            ;;[cljfx.controls :as controls]
-            ;;[cljfx.stylesheet :as style]
             [fcb-buddy.midi :as midi]
             [fcb-buddy.prefs :as prefs]
             [fcb-buddy.utils :as utils]
@@ -17,128 +13,49 @@
   (:import [fcb_buddy MidiFootController Utils])
   (:gen-class))
 
-(defn- file-open [e]
-  nil)
+(comment
+  (let [root "https://mediadl.musictribe.com/media/PLM/data/docs/P0089/"]
+    (defn- help-man [e]
+      (letfn [(url [lang]
+                (str root
+                     "FCB1010_P0089_M_"
+                     lang
+                     ".pdf"))
+              (language-dialog []
+                (input "Choose a language."
+                       :choices [{:lang "Chinese"    :url "CN"}
+                                 {:lang "Danish"     :url "DK"}
+                                 {:lang "Dutch"      :url "NL"}
+                                 {:lang "English"    :url "EN"}
+                                 {:lang "Finnish"    :url "FI"}
+                                 {:lang "French"     :url "FR"}
+                                 {:lang "German"     :url "DE"}
+                                 {:lang "Greek"      :url "GR"}
+                                 {:lang "Italian"    :url "IT"}
+                                 {:lang "Japanese"   :url "JP"}
+                                 {:lang "Portuguese" :url "PT"}
+                                 {:lang "Spanish"    :url "ES"}
+                                 {:lang "Swedish"    :url "SE"}]
+                       :to-string :lang))]
+        (-> (language-dialog)
+            :url
+            url
+            browse-url)))
+    (defn- help-quickstart [e]
+      (let [f "QSG_BE_0715-AAA_FCB1010_WW.pdf"]
+        (browse-url (str root f)))))
 
-(defn- file-quit [e]
-  (invoke-later (dispose! e)))
-
-(defn- edit-prefs [e]
-  nil)
-
-(defn- help-docs [e]
-  (let [docs-url ""]
-    (browse-url docs-url)))
-
-(let [root "https://mediadl.musictribe.com/media/PLM/data/docs/P0089/"]
-  (defn- help-man [e]
-    (letfn [(url [lang]
-              (str root
-                   "FCB1010_P0089_M_"
-                   lang
-                   ".pdf"))
-            (language-dialog []
-              (input "Choose a language."
-                     :choices [{:lang "Chinese"    :url "CN"}
-                               {:lang "Danish"     :url "DK"}
-                               {:lang "Dutch"      :url "NL"}
-                               {:lang "English"    :url "EN"}
-                               {:lang "Finnish"    :url "FI"}
-                               {:lang "French"     :url "FR"}
-                               {:lang "German"     :url "DE"}
-                               {:lang "Greek"      :url "GR"}
-                               {:lang "Italian"    :url "IT"}
-                               {:lang "Japanese"   :url "JP"}
-                               {:lang "Portuguese" :url "PT"}
-                               {:lang "Spanish"    :url "ES"}
-                               {:lang "Swedish"    :url "SE"}]
-                     :to-string :lang))]
-      (-> (language-dialog)
-          :url
-          url
-          browse-url)))
-  (defn- help-quickstart [e]
-    (let [f "QSG_BE_0715-AAA_FCB1010_WW.pdf"]
-      (browse-url (str root f)))))
-
-(defn- help-about [e]
-  (invoke-later
-    (-> (dialog :content
-                (str/join "\n"
-                          [(let [project-version (:fcb-buddy-version env)]
-                             (str "FCB Buddy - Version " project-version))
-                           "Programming and configuration tool for FCB1010 MIDI Foot Controller."]))
-        pack!
-        show!)))
-
-;; TODO: Fix since map order is not guarunteed.
-(def menu-items
-  {:file-open (action :handler file-open :name "Open" :tip "Open SYSEX file." :key "menu O")
-   :file-sep1 (separator)               ; First file menu seperator
-   :file-quit (action :handler file-quit :name "Quit" :tip "Quit FCB Buddy." :key "menu Q")
-   :edit-prefs (action :handler edit-prefs :name "Preferences" :tip "Edit the user preferences." :key "menu P")
-   :help-docs (action :handler help-docs :name "Documentation" :tip "View the online documentation." :key "F1")
-   :help-man (action :handler help-man :name "FCB1010 Manual" :tip "View the FCB1010 manual.")
-   :help-quickstart (action :handler help-quickstart :name "FCB1010 Quickstart" :tip "View the FCB1010 quickstart manual.")
-   :help-sep1 (separator)               ; First help menu separator
-   :help-about (action :handler help-about :name "About" :tip "")})
-
-(defn- get-menu-items
-  [prefix]
-  (vec (vals (utils/filter-by-prefix menu-items prefix))))
-
-(defn make-frame
-  []
-  (frame
-   :title "FCB Buddy"
-   :size [640 :by 480]
-   ;;:on-close :exit
-   :menubar (menubar
-             :items [(menu :text "File" :items (get-menu-items :file))
-                     (menu :text "Edit" :items (get-menu-items :edit))
-                     (menu :text "MIDI" :items (get-menu-items :midi))
-                     (menu :text "Tools" :items (get-menu-items :tools))
-                     (menu :text "Help" :items (get-menu-items :help))])
-   :content
-   (tabbed-panel :placement :top
-                 :overflow :scroll
-                 :tabs [{:title "Presets"
-                         :tip "Edit presets."
-                         :content "Hello, world!"}
-                        {:title "MIDI Messages"
-                         :tip "View MIDI message log."
-                         :content (border-panel
-                                   :north (scrollable (log-window :id :log-window
-                                                                  :limit nil
-                                                                  ))
-                                   :south (horizontal-panel :items [(button :id :log-resume :text "Resume")
-                                                                    (button :id :log-pause :text "Pause")]))}])))
-
-(defn- logger
-  [lw message go]
-  (log lw (str (java.util.Date.) message)))
-
-(defn- add-behaviors
-  [frame]
-  (let [{:keys [log-window start stop limit limit?]} (group-by-id frame)]
-    (listen
-     limit?
-     :selection (fn [_] (config! log-window :limit (if (value limit?)
-                                                     (value limit)))))
-    (listen stop
-            :action (fn [_] nil))
-    (listen
-     start
-     :action (fn [_]
-               (logger log-window "Hello, world!" true))))
-  frame)
-
-(def renderer
-  (fx/create-renderer))
+  (defn- get-menu-items
+    [prefix]
+    (vec (vals (utils/filter-by-prefix menu-items prefix)))))
+;;; Stores application state.
+(defrecord State [prefs])
 
 (def *state
   (atom true))
 
+(def renderer
+  (fx/create-renderer))
 (def *context
   (atom
    (fx/create-context {}
@@ -150,6 +67,7 @@
   (prn e))
 
 (defmethod event-handler ::close [_]
+  (println "Closing")
   (reset! *state false))
 
 (def menu-actions
@@ -158,7 +76,9 @@
    :edit {:prefs (fn [_])}
    :midi nil
    :tools nil
-   :help {:docs (fn [_])
+   :help {:docs (fn [_]
+                  (let [docs-url ""]
+                    (browse-url docs-url)))
           :man (fn [_])
           :quick-start (fn [_])
           :about (fn [_]
@@ -175,7 +95,12 @@
                                               "Programming and configuration tool for FCB1010 MIDI Foot Controller."])
                                    :button-types [:close]}}))}})
 
-(defn- menu-bar [_]
+(defn- separator-menu-item [_]
+  {:fx/type :custom-menu-item
+   :disable true
+   :content {:fx/type :separator}})
+
+(defn- menu-bar-view [_]
   (letfn [(get-action [& ks]
             (get-in menu-actions ks))]
     {:fx/type :menu-bar
@@ -185,8 +110,7 @@
                        :text "Open SYSEX"
                        :accelerator "Shortcut+O"
                        :on-action (get-action :file :open)}
-                      {:fx/type :menu-item
-                       :disable true}
+                      {:fx/type separator-menu-item}
                       {:fx/type :menu-item
                        :text "Quit"
                        :accelerator "Shortcut+Q"
@@ -215,13 +139,12 @@
                       {:fx/type :menu-item
                        :text "FCB1010 Quickstart"
                        :on-action (get-action :help :quick-start)}
-                      {:fx/type :menu-item
-                       :disable true}
+                      {:fx/type separator-menu-item}
                       {:fx/type :menu-item
                        :text "About"
                        :on-action (get-action :help :about)}]}]}))
 
-(defn- root [_]
+(defn- root-view [_]
   {:fx/type :stage
    :showing true
    :title "FCB Buddy"
@@ -230,36 +153,45 @@
    :scene {:fx/type :scene
            :stylesheets #{(utils/resource-path "css/styles.css")}
            :root {:fx/type :v-box
-                  :children [{:fx/type menu-bar}
-                             {:fx/type :tab-pane
-                              :pref-width 960
-                              :pref-height 540
-                              :tabs [{:fx/type :tab
-                                      :text "Presets"
-                                      :closable false
-                                      :content {:fx/type :label
-                                                :text "Hello, world!"}}
-                                     {:fx/type :tab
-                                      :text "MIDI Messages"
-                                      :closable false
-                                      :content {:fx/type :label
-                                                :text "Hello, world!"}}]}]}}})
+                  :children
+                  [{:fx/type menu-bar-view}
+                   {:fx/type :tab-pane
+                    :pref-width 960
+                    :pref-height 540
+                    :tabs [{:fx/type :tab
+                            :text "Presets"
+                            :closable false
+                            :content {:fx/type :label
+                                      :text "Hello, world!"}}
+                           {:fx/type :tab
+                            :text "MIDI Messages"
+                            :closable false
+                            :content {:fx/type :border-pane
+                                      :center {:fx/type :text-area
+                                               :editable false
+                                               :text (str/join "\n" (take 25 (repeat "Hello, world!")))}
+                                      :bottom {:fx/type :h-box
+                                               :padding {:top 15.0
+                                                         :bottom 15.0
+                                                         :left 15.0
+                                                         :right 15.0}
+                                               :spacing 5.0
+                                               :children [{:fx/type :button
+                                                           :text "Resume"}
+                                                          {:fx/type :button
+                                                           :text "Pause"}]}}}]}]}}})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (comment (invoke-later
-             (-> (make-frame)
-                 ;;add-behaviors
-                 show!)))
   (fx/create-app
    *context
    :event-handler event-handler
-   :desc-fn root
-   :renderer-middleware identity ; TODO: Custom middleware.
+   :desc-fn root-view
+   :renderer-middleware identity ; TODO: Add custom middleware (identity is the default).
    :renderer-error-handler (fn [e]
                              (if (instance? Exception e)
-                               (fx/create-component
+                               (fx/create-component ; Display exceptions in an alert dialog
                                 {:fx/type :alert
                                  :alert-type :error
                                  :showing true
@@ -271,4 +203,3 @@
                                                :button-types [(javafx.scene.control.ButtonType. "Quit")
                                                               (javafx.scene.control.ButtonType. "Continue")]}})
                                (throw e)))))
-
--- src/clojure/fcb_buddy/prefs.clj
+++ src/clojure/fcb_buddy/prefs.clj
@@ -1,9 +1,21 @@
 (ns fcb-buddy.prefs
   "Application preferences frames & utilities."
-  (:use [seesaw.core])
   (:require [cljfx.api :as fx])
   (:import [fcb_buddy MidiFootController]
            [javax.sound.midi MidiSystem]))
 
+(defrecord Preferences [])
 
+(defn load-config
+  []
+  (letfn [(find-cfg []
+            )]
+    ))
 
+(defn save-config
+  []
+  )
+
+(defn root-view
+  []
+  )
